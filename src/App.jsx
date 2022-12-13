d7028e7a9649841b401a9f7384c8d9be91bf607b
Johnny <johnny_s.o@hotmail.com>
2022-12-13 07:58:49 -0300

    Adicionando section de promoções

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import Promo from './components/Promo/Promo';
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <Promo />
       <h1>Hello World</h1>
     </div>
   )
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Promo.css'
+
+function Promo(){
+  return (
+    <section className="promotions">
+    </section>
+  )
+}
+
+export default Promo;
