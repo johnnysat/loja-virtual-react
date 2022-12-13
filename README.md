a785822e76979123431000d802c49abbaba5244b
Johnny <johnny_s.o@hotmail.com>
2022-12-13 07:16:39 -0300

    Adicionado planejamento ao Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# loja-virtual-react
+# Loja Virtual
+
+## Planejamento
+[]Construir o header com menu de navegação.
+[]Construir o body em grid-layout com cards dos itens a venda.
+[]Construir aside com anuncio de outras opções. 
+[]Construir footer.
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
