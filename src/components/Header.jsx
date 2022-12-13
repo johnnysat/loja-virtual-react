3ac63f09534b89c33fcffa09ee3d2cb369f62ab7
Johnny <johnny_s.o@hotmail.com>
2022-12-13 07:34:54 -0300

    Header com link na Home e UL na navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 function Header(){
   return(
-    <>
-    </>
+    <div className="navbar">
+    <a src="#">Home</a>
+    <ul>
+      <li>Sobre</li>
+      <li>Contato</li>
+      <li>Carrinho</li>
+    </ul>
+    </div>
   )
 }
 
