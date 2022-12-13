2215e190070a0f935cfb01ca6a60a2861d9fb07e
Johnny <johnny_s.o@hotmail.com>
2022-12-13 09:05:43 -0300

    Adicionando imagem ao banner da section promotions

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './Promo.css'
+import banner from '../../images/Genshin_Impact_Key_Art-EN.jpg'
 
 function Promo(){
   return (
     <section className="promotions">
+      <img src={banner}/>
     </section>
   )
 }
