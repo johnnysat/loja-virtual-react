ac5da448254a4757e2129abfbff32b5a44af60bb
Johnny <johnny_s.o@hotmail.com>
2022-12-26 06:48:38 -0300

    Arrumando imagem da Alloy que estava com escrita

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -7,11 +7,6 @@ function Aside() {
     <h3>Filtre por Visão</h3>
     <ul className="charList">
       <li>Pyro</li>
-      <li>Hydro</li>
-      <li>Electro</li>
-      <li>Anemo</li>
-      <li>Cryo</li>
-      <li>Dendro</li>
     </ul>
     </aside>
   )
